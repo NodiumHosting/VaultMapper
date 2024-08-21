ffd9be991809b492bc7642100e5e8bb180f2c0c1
Anorak01 <adamfri007@gmail.com>
2024-08-21 12:25:59 +0200

    Optimized overlay rendering (needs more work)

--- src/main/java/com/nodiumhosting/vaultmapper/VaultMapper.java
+++ src/main/java/com/nodiumhosting/vaultmapper/VaultMapper.java
@@ -2,11 +2,16 @@ package com.nodiumhosting.vaultmapper;
 
 import com.mojang.logging.LogUtils;
 import com.nodiumhosting.vaultmapper.commands.VaultMapperCommand;
+import com.nodiumhosting.vaultmapper.map.VaultMapOverlayRenderer;
+import net.minecraft.client.Minecraft;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.lwjgl.glfw.GLFW;
+import org.lwjgl.glfw.GLFWWindowSizeCallback;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -33,5 +38,17 @@ public class VaultMapper
         public static void registerCommands(RegisterCommandsEvent event){
             VaultMapperCommand.register(event.getDispatcher());
         }
+    }
+
+    @SubscribeEvent
+    public static void onClientSetup(FMLClientSetupEvent event) {
+        var handle = Minecraft.getInstance().getWindow().getWindow();
+        GLFW.glfwSetWindowSizeCallback(handle, new GLFWWindowSizeCallback() {
+            @Override
+            public void invoke(long window, int width, int height) {
+                VaultMapOverlayRenderer.onWindowResize(window, width, height);
+            }
+        });
+        VaultMapOverlayRenderer.prep();
     }
 }
--- src/main/java/com/nodiumhosting/vaultmapper/map/VaultMapOverlayRenderer.java
+++ src/main/java/com/nodiumhosting/vaultmapper/map/VaultMapOverlayRenderer.java
@@ -1,5 +1,6 @@
 package com.nodiumhosting.vaultmapper.map;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiComponent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -13,30 +14,19 @@ import java.util.*;
 public class VaultMapOverlayRenderer {
     public static boolean enabled = false;
 
+    public static HashMap<Integer, Integer> roomColors = new HashMap<>();
+
+    static int importantRoomColor = 0xFFFF00FF;
+
+    static int mapStartX;
+    static int mapStartY;
+    static int mapRoomWidth;
+
+
+
     @SubscribeEvent(priority = EventPriority.NORMAL)
     public static void eventHandler(RenderGameOverlayEvent.Pre event) {
         if (!enabled) return;
-
-        int w = event.getWindow().getGuiScaledWidth();
-        int h = event.getWindow().getGuiScaledHeight();
-
-        int mapWidth = (int) (w * 0.25f);
-        int mapHeight = (int) (w * 0.25f);
-        int mapStartX = w - mapWidth;
-        int mapStartY = h - mapHeight;
-
-        int mapRoomWidth = mapWidth / 49;
-
-        HashMap<Integer, Integer> roomColors = new HashMap<>();
-        roomColors.put(0, 0xFF000000); // void
-        roomColors.put(1, 0xFF0000FF); // tunnelX
-        roomColors.put(2, 0xFF0000FF); // tunnelZ
-        roomColors.put(3, 0xFF0000FF); // room
-        roomColors.put(4, 0xFFFF0000); // start
-        roomColors.put(5, 0xFF00FF00); // current
-        roomColors.put(6, 0xFFFFD700); // inscription
-
-        int importantRoomColor = 0xFFFF00FF;
 
         //draw map grid
         for (int x = -24; x <= 24; x++) {
@@ -55,5 +45,35 @@ public class VaultMapOverlayRenderer {
                 if (roomVisited == 2 || roomVisited == 3) GuiComponent.fill(event.getMatrixStack(), mapX - 1, mapZ - 1, mapX + 1, mapZ + 1, importantRoomColor);
             }
         }
+    }
+
+    public static void onWindowResize(long window, int width, int height) {
+        int mapWidth = (int) (width * 0.25f);
+        int mapHeight = (int) (width * 0.25f); // this seems weird but it was similar in original implementation
+        mapStartX = width - mapWidth;
+        mapStartY = height - mapHeight;
+
+        mapRoomWidth = mapWidth / 49;
+    };
+
+    public static void prep() {
+        roomColors.put(0, 0xFF000000); // void
+        roomColors.put(1, 0xFF0000FF); // tunnelX
+        roomColors.put(2, 0xFF0000FF); // tunnelZ
+        roomColors.put(3, 0xFF0000FF); // room
+        roomColors.put(4, 0xFFFF0000); // start
+        roomColors.put(5, 0xFF00FF00); // current
+        roomColors.put(6, 0xFFFFD700); // inscription
+
+
+        int w = Minecraft.getInstance().getWindow().getGuiScaledWidth();
+        int h = Minecraft.getInstance().getWindow().getGuiScaledHeight();
+
+        int mapWidth = (int) (w * 0.25f);
+        int mapHeight = (int) (w * 0.25f);
+        mapStartX = w - mapWidth;
+        mapStartY = h - mapHeight;
+
+        mapRoomWidth = mapWidth / 49;
     }
 }
