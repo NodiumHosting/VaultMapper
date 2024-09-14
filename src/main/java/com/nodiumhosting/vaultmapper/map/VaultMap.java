eb8e50792b687bbcc8d96ba1e859d3239c8148e5
McMelonTV <mcmelontube@gmail.com>
2024-09-15 00:20:01 +0200

    fix oopsie

--- src/main/java/com/nodiumhosting/vaultmapper/map/VaultMap.java
+++ src/main/java/com/nodiumhosting/vaultmapper/map/VaultMap.java
@@ -172,7 +172,7 @@ public class VaultMap {
 
         if (isWithinBounds(playerRoomX, playerRoomZ)) {
             if (debug) {
-                Minecraft.getInstance().gui.setOverlayMessage(new TextComponent("Current room: " + playerRoomX + ", " + playerRoomZ + " Hologram: " + (hologramData != null ? "Found" : "Not found") + (hologramChecked ? " (Checked)" : "(Not checked)") + " Vault Map Data Size: " + cells.size() + " (" + cells.stream().filter(cell -> cell.cellType == CellType.ROOM && cell.explored) + " Explored Rooms)"), false);
+                Minecraft.getInstance().gui.setOverlayMessage(new TextComponent("Current room: " + playerRoomX + ", " + playerRoomZ + " Hologram: " + (hologramData != null ? "Found" : "Not found") + (hologramChecked ? " (Checked)" : "(Not checked)") + " Vault Map Data Size: " + cells.size() + " (" + cells.stream().filter(cell -> cell.cellType == CellType.ROOM && cell.explored).count() + " Explored Rooms)"), false);
             }
             if (!isCurrentRoom(playerRoomX, playerRoomZ)) { // if were in a different room
                 updateMap();
