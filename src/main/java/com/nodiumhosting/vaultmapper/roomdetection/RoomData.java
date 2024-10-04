bbd9f38c375dc21ad9f09a64a8800d1877723800
joseph <urmomafrog@gmail.com>
2024-10-04 19:24:20 +0300

    STORING OF OMEGA AND CHALLENGE ROOMS DONE

--- src/main/java/com/nodiumhosting/vaultmapper/roomdetection/RoomData.java
+++ src/main/java/com/nodiumhosting/vaultmapper/roomdetection/RoomData.java
@@ -9,20 +9,29 @@ import iskallia.vault.core.data.key.registry.KeyRegistry;
 import iskallia.vault.core.util.WeightedList;
 import iskallia.vault.core.vault.Vault;
 import iskallia.vault.core.vault.VaultRegistry;
+import iskallia.vault.core.world.data.entity.PartialCompoundNbt;
+import iskallia.vault.core.world.data.tile.PartialBlockState;
+import iskallia.vault.core.world.data.tile.PartialTile;
+import iskallia.vault.core.world.data.tile.TilePredicate;
 import iskallia.vault.core.world.generator.GridGenerator;
 import iskallia.vault.core.world.generator.layout.ClassicVaultLayout;
+import iskallia.vault.core.world.processor.ProcessorContext;
+import iskallia.vault.core.world.template.PlacementSettings;
 import iskallia.vault.core.world.template.Template;
 import iskallia.vault.core.world.template.data.DirectTemplateEntry;
 import iskallia.vault.core.world.template.data.IndirectTemplateEntry;
 import iskallia.vault.core.world.template.data.TemplatePool;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.BlockGetter;
 
 import java.util.*;
 
 public class RoomData {
 
-    public static List<RoomData> omegaRooms;
-    public static List<RoomData> challengeRooms;
+    public static List<RoomData> omegaRooms = new ArrayList<>();
+    public static List<RoomData> challengeRooms = new ArrayList<>();
     public static void initRooms() {
         TemplatePoolKey challengeRef = VaultRegistry.TEMPLATE_POOL.getKey("the_vault:vault/rooms/challenge_rooms");
         TemplatePoolKey omegaRef = VaultRegistry.TEMPLATE_POOL.getKey("the_vault:vault/rooms/omega_rooms");
@@ -105,15 +114,35 @@ public class RoomData {
     public String name;
     public String simpleName;
     public Template room;
-    public List<String> northeastColumn;
-    public List<String> northwestColumn;
-    public List<String> southeastColumn;
-    public List<String> southwestColumn;
+    public List<String> northeastColumn = new ArrayList<>();
+    public List<String> northwestColumn = new ArrayList<>();
+    public List<String> southeastColumn = new ArrayList<>();
+    public List<String> southwestColumn = new ArrayList<>();
 
     public RoomData(String type, String simpleName, String name, Template room) {
         this.type = type;
+        this.simpleName = simpleName;
         this.name = name;
         this.room = room;
+        Iterator<PartialTile> tiles = room.getTiles(Template.ALL_TILES);
+        while (tiles.hasNext()) {
+            PartialTile tile = tiles.next();
+            BlockPos pos = tile.getPos();
+            if (pos.getX() == 0 && pos.getZ() == 0) {
+                northwestColumn.add(tile.getState().getBlock().toString());
+            }
+            if (pos.getX() == 46 && pos.getZ() == 0) {
+                northeastColumn.add(tile.getState().getBlock().toString());
+            }
+            if (pos.getX() == 0 && pos.getZ() == 46) {
+                southwestColumn.add(tile.getState().getBlock().toString());
+            }
+            if (pos.getX() == 46 && pos.getZ() == 46) {
+                southeastColumn.add(tile.getState().getBlock().toString());
+                VaultMapper.LOGGER.info(tile.getState().getBlock().toString());
+            }
+        }
     }
+
 
 }
