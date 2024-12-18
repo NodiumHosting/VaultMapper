786b28b5251150211aa9ca339faa314e6de03d2e
Anorak01 <adamfri007@gmail.com>
2024-12-19 00:42:03 +0100

    Minor cleanup

--- src/main/java/com/nodiumhosting/vaultmapper/sync/WSClient.java
+++ src/main/java/com/nodiumhosting/vaultmapper/sync/WSClient.java
@@ -57,19 +57,19 @@ public class WSClient extends WebSocketClient {
     public void onMessage(String message) {
 //        VaultMapper.LOGGER.info(message); // log the json
         try {
-            var x = new GsonBuilder().create().fromJson(message, Capsule.class);
-            if (x.type.equals(String.valueOf(PacketType.MOVE.getValue()))) {
-                MovePacket movePacket = new GsonBuilder().create().fromJson(x.data, MovePacket.class);
+            var dataCapsule = new GsonBuilder().create().fromJson(message, Capsule.class);
+            if (dataCapsule.type.equals(String.valueOf(PacketType.MOVE.getValue()))) {
+                MovePacket movePacket = new GsonBuilder().create().fromJson(dataCapsule.data, MovePacket.class);
 
                 VaultMap.updatePlayerMapData(movePacket.uuid, movePacket.color, movePacket.x, movePacket.z, movePacket.yaw);
                 VaultMapper.wsServer.sendArrow(movePacket.x, movePacket.z, movePacket.yaw, movePacket.uuid, movePacket.color);
-            } else if (x.type.equals(String.valueOf(PacketType.CELL.getValue()))) {
-                VaultCell cellPacket = new GsonBuilder().create().fromJson(x.data, VaultCell.class); //have to change maybe
+            } else if (dataCapsule.type.equals(String.valueOf(PacketType.CELL.getValue()))) {
+                VaultCell cellPacket = new GsonBuilder().create().fromJson(dataCapsule.data, VaultCell.class); //have to change maybe
 
                 VaultMap.addOrReplaceCell(cellPacket);
                 VaultMapper.wsServer.sendCell(cellPacket);
-            } else if (x.type.equals(String.valueOf(PacketType.LEAVE.getValue()))) {
-                LeavePacket leavePacket = new GsonBuilder().create().fromJson(x.data, LeavePacket.class);
+            } else if (dataCapsule.type.equals(String.valueOf(PacketType.LEAVE.getValue()))) {
+                LeavePacket leavePacket = new GsonBuilder().create().fromJson(dataCapsule.data, LeavePacket.class);
 
                 VaultMap.removePlayerMapData(leavePacket.uuid);
                 VaultMapper.wsServer.removeArrow(leavePacket.uuid);
