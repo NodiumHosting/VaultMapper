fa95c2b16092c09228c52068aafb00d01046962f
McMelonTV <mcmelontube@gmail.com>
2024-12-30 10:52:12 +0100

    fix gradl mayb

--- src/main/java/com/nodiumhosting/vaultmapper/mixin/OptionsScreenConfigButtonMixin.java
+++ src/main/java/com/nodiumhosting/vaultmapper/mixin/OptionsScreenConfigButtonMixin.java
@@ -12,7 +12,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(value = OptionsScreen.class, remap = false)
+@Mixin(value = OptionsScreen.class, remap = true)
 public class OptionsScreenConfigButtonMixin extends Screen {
     protected OptionsScreenConfigButtonMixin(Component pTitle) {
         super(pTitle);
