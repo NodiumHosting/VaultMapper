6d19daebb9eb26b8e3b0345b47f87e47cfcc82c1
mel <mcmelon@nodiumhosting.com>
2024-12-30 02:08:08 +0100

    E

--- src/main/java/com/nodiumhosting/vaultmapper/util/Clamp.java
+++
@@ -1,7 +0,0 @@
-package com.nodiumhosting.vaultmapper.util;
-
-public class Clamp {
-    public static int clamp(int value, int min, int max) {
-        return Math.max(min, Math.min(max, value));
-    }
-}
